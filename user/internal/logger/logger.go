e842ca7d526e56e21f6c8321ccb53debe2f4002d
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2024-09-26 01:00:18 -0500

    feat: Add Docker Compose configuration for server and MySQL
    - Added a new Docker Compose configuration file to set up the server and MySQL containers.
    - The server container is built using the final target in the user directory.
    - The server container exposes port 8080 and depends on the MySQL container.
    - Environment variables are set for the server container to connect to the MySQL database.
    - The MySQL container is configured with the necessary environment variables and port mapping.
    - A volume is created for the MySQL container to persist data.
    
    refactor: Update .dockerignore file
    
    - Added new entries to the .dockerignore file to exclude unnecessary files and directories from being copied to the container.
    
    feat: Add Dockerfile for server
    
    - Added a Dockerfile for building the server container.
    - The Dockerfile uses the golang base image and sets the Go version to 1.23.1.
    - Dependencies are downloaded separately to take advantage of Docker's caching.
    - The application is built for the specified target architecture.
    - The final stage uses the alpine image as the base for running the application.
    - Runtime dependencies are installed, including ca-certificates and tzdata.
    - A non-privileged user is created to run the application.
    - The server executable is copied from the build stage to the final stage.
    - The container exposes port 8080 and specifies the entrypoint for the application.
    
    refactor: Update logger package
    
    - Added error handling to the InitLogger function in the logger package.
    - If a connection cannot be established to Logstash, a warning message is logged.
    - Otherwise, a JSON handler is used to log messages to Logstash.

--- user/internal/logger/logger.go
+++ user/internal/logger/logger.go
@@ -2,13 +2,23 @@ package logger
 
 import (
 	"log/slog"
+	"net"
 	"os"
 )
 
 var Logger *slog.Logger
 
 func InitLogger() {
-	Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	conn, err := net.Dial("tcp", "localhost:5044")
+	if err != nil {
+		Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo, // Set log level
+		}))
+		Logger.Warn("Failed to connect to Logstash", slog.String("error", err.Error()))
+		return
+	}
+
+	Logger = slog.New(slog.NewJSONHandler(conn, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
 	}))
 }
